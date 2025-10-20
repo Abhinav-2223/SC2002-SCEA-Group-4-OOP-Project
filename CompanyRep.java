26d474b8f25cc3a75e9f32cf71c886863e92e4ea
Abhinav-2223 <narneabhinav123@gmail.com>
2025-10-20 17:49:57 +0800

    created initial User class and added the method names to CompanyRep class

--- CompanyRep.java
+++ CompanyRep.java
@@ -1,4 +1,39 @@
 
 public class CompanyRep {
 
+    // instance variables
+    private String companyName;
+    private boolean isApproved;
+
+
+    // methods
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void registerCompany(String companyName) {
+        if (isApproved) {
+            System.out.println("Company is already registered and approved.");
+        } else {
+            this.companyName = companyName;
+            this.isApproved = false; // registration pending approval
+            System.out.println("Company registration submitted for approval.");
+        }
+    }
+
+    public void createinternships(){
+
+    }
+
+    public void approveRegistration(){
+
+    }
+
+    public void toggleInternshipVisibility(){
+
+    }
+
+    
+
 }
--- User.java
+++ User.java
@@ -1,3 +1,72 @@
 public abstract class User {
-    
+
+    // instance variables
+    private String userid;
+    private String password;
+    private String name;
+
+    // methods
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void signup(String userid, String password, String name) {
+        // use the setter methods to set the instance variables
+        this.setUserid(userid);
+        this.setPassword(password);
+        this.setName(name);
+        System.out.println("Signup successful!");
+    }
+
+
+    public void login(String userid, String password) {
+        // use the getter methods to access the instance variables
+        if (this.getUserid().equals(userid) && this.getPassword().equals(password)) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Invalid userid or password.");
+        }
+        
+    }
+
+    public void logout() {
+        System.out.println("Logout successful!");
+    }
+
+    public void changePassword(String newPassword) {
+        this.setPassword(newPassword);
+        System.out.println("Password changed successfully!");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
