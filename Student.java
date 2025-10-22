720d8f405ba2b386256dab29f2a3f2d6a56fc16f
Abhinav-2223 <narneabhinav123@gmail.com>
2025-10-23 04:33:43 +0800

    started Student class

--- Student.java
+++ Student.java
@@ -1,3 +1,72 @@
+
+
 public class Student {
     
+    // instance variables
+    private String studentId;
+    private String name;
+    private String major;
+    private int studyYear;
+
+    // constructor
+    public Student(String studentId, String name, String major, int studyYear) {
+        this.studentId = studentId;
+        this.name = name;
+        this.major = major;
+        this.studyYear = studyYear;
+    }
+
+    // getters and setters
+    public String getStudentId() {
+        return studentId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public int getStudyYear() {
+        return studyYear;
+    }
+
+    // methods 
+
+    // Student can view the list of available internships
+    public void viewInternshipList() {
+       
+
+    }
+
+
+    // Student can apply for an internship
+    public void applyForInternship(Internships internship) {
+        
+    }
+
+    // Student can view the status of their applications
+    public void viewApplicationStatus() {
+        
+    }
+    
+    // Student can accept an internship placement offer
+    public void acceptInternshipPlacement(Internships internship) {
+        
+    }
+
+
+
+    // Student can withdraw their application
+    public void requestWithdrawal(Internships internship) {
+        
+    }
+
+    
+
+
+
+
+
+
+
 }
