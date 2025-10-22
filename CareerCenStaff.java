cab418204be7096a612c975dff623d815e05ec19
Reyes <pohyd02@gmail.com>
2025-10-22 14:15:29 +0800

    Branch Clean up

--- CareerCenStaff.java
+++ CareerCenStaff.java
@@ -1,42 +1,42 @@
-public class CareerCenStaff extends User{
-    //fields
-    private String staffID;
-
-    //methods
-
-    public String getStaffID() {
-        return staffID;
-    }
-
-    public void setStaffID(String ID) {
-        staffID = ID;
-    }
-
-    public void approveAccountCreationofCompRep(CompanyRep companyRep) {
-        companyRep.setIsApproved(true);
-    }
-
-
-    public void approveIntern(CompanyRep rep, int index ) {
-        Internship internship = rep.getInternships().get(index);
-        internship.setApplicationStatus(ApplicationStatus.SUCCESSFUL);
-    }
-
-    public void approveWithdrawalRequest(Student student, WithdrawalDecision decision) {
-        student.getStudentApplication().setWithdrawalDecision(decision);  //same thing as before
-    }
-
-    public void generateReport(Student student) {
-        System.out.println("Student application ID: " + student.getStudentApplication().getApplicationID());
-        System.out.println("Student name: " + student.getUserID());
-        System.out.println("Number of internships applied: " + student.getStudentApplication().getInternships().size());
-        if (!student.getStudentApplication().getInternships().isEmpty()) {
-            for (int i = 0; i < student.getStudentApplication().getInternships().size(); i++) {
-                Internship internship = student.getStudentApplication().getInternships().get(i);
-                System.out.println((i + 1) + ". " + internship.getTitle() + " - Status: " + internship.getApplicationStatus());
-            }
-        } else {
-            System.out.println("No internships applied yet.");
-        }
-    }
-}
+//public class CareerCenStaff extends User{
+//    //fields
+//    private String staffID;
+//
+//    //methods
+//
+//    public String getStaffID() {
+//        return staffID;
+//    }
+//
+//    public void setStaffID(String ID) {
+//        staffID = ID;
+//    }
+//
+//    public void approveAccountCreationofCompRep(CompanyRep companyRep) {
+//        companyRep.setIsApproved(true);
+//    }
+//
+//
+//    public void approveIntern(CompanyRep rep, int index ) {
+//        Internship internship = rep.getInternships().get(index);
+//        internship.setApplicationStatus(ApplicationStatus.SUCCESSFUL);
+//    }
+//
+//    public void approveWithdrawalRequest(Student student, WithdrawalDecision decision) {
+//        student.getStudentApplication().setWithdrawalDecision(decision);  //same thing as before
+//    }
+//
+//    public void generateReport(Student student) {
+//        System.out.println("Student application ID: " + student.getStudentApplication().getApplicationID());
+//        System.out.println("Student name: " + student.getUserID());
+//        System.out.println("Number of internships applied: " + student.getStudentApplication().getInternships().size());
+//        if (!student.getStudentApplication().getInternships().isEmpty()) {
+//            for (int i = 0; i < student.getStudentApplication().getInternships().size(); i++) {
+//                Internship internship = student.getStudentApplication().getInternships().get(i);
+//                System.out.println((i + 1) + ". " + internship.getTitle() + " - Status: " + internship.getApplicationStatus());
+//            }
+//        } else {
+//            System.out.println("No internships applied yet.");
+//        }
+//    }
+//}
--- CompanyRep.java
+++ CompanyRep.java
@@ -1,59 +1,59 @@
-import java.util.List;
-import java.util.ArrayList;
-
-public class CompanyRep extends User{
-
-    // instance variables
-    private String companyName;
-    private boolean isApproved;
-    private List<Internships> internships;
-
-    public CompanyRep() {
-        this.internships = new ArrayList<>();
-    }
-
-    // methods
-    public boolean getIsApproved() {
-        return isApproved;
-    }
-    public void setIsApproved(boolean isApproved) {
-        this.isApproved = isApproved;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void registerCompany(String companyName) {
-        if (isApproved) {
-            System.out.println("Company is already registered and approved.");
-        } else {
-            this.companyName = companyName;
-            this.isApproved = false; // registration pending approval
-            System.out.println("Company registration submitted for approval.");
-        }
-    }
-
-    public void createInternships(){
-
-    }
-
-    public void approveRegistration(){
-
-    }
-
-    public void toggleInternshipVisibility(Internships internship){
-        internship.toggleVisibility();
-    }
-
-    public List<Internships> getInternships(){
-        return internships;
-    }
-
-    public void viewInternship(Internships internship){
-        //need do StudentApplication first then get information from there
-    }
-
-    //need implement another method to approve or reject student's application
-
-}
+//import java.util.List;
+//import java.util.ArrayList;
+//
+//public class CompanyRep extends User{
+//
+//    // instance variables
+//    private String companyName;
+//    private boolean isApproved;
+//    private List<Internships> internships;
+//
+//    public CompanyRep() {
+//        this.internships = new ArrayList<>();
+//    }
+//
+//    // methods
+//    public boolean getIsApproved() {
+//        return isApproved;
+//    }
+//    public void setIsApproved(boolean isApproved) {
+//        this.isApproved = isApproved;
+//    }
+//
+//    public String getCompanyName() {
+//        return companyName;
+//    }
+//
+//    public void registerCompany(String companyName) {
+//        if (isApproved) {
+//            System.out.println("Company is already registered and approved.");
+//        } else {
+//            this.companyName = companyName;
+//            this.isApproved = false; // registration pending approval
+//            System.out.println("Company registration submitted for approval.");
+//        }
+//    }
+//
+//    public void createInternships(){
+//
+//    }
+//
+//    public void approveRegistration(){
+//
+//    }
+//
+//    public void toggleInternshipVisibility(Internships internship){
+//        internship.toggleVisibility();
+//    }
+//
+//    public List<Internships> getInternships(){
+//        return internships;
+//    }
+//
+//    public void viewInternship(Internships internship){
+//        //need do StudentApplication first then get information from there
+//    }
+//
+//    //need implement another method to approve or reject student's application
+//
+//}
