f983374e1ad6165fd2a33242c54d03d1e4897c5d
rayson123inc <echobolt456@gmail.com>
2025-11-02 20:24:49 +0800

    Change CareerCenStaff to fit more inline with StudentApp & CompanyRep class.
    StudentApplication class needs to instantiate Internship as a array so that generateReport method can access each of them!

--- CareerCenStaff.java
+++ CareerCenStaff.java
@@ -2,21 +2,12 @@ import enums.OpportunityStatus;
 import enums.WithdrawalDecision;
 
 public class CareerCenStaff extends User{
-    public CareerCenStaff() {
 
-    }
+    //fields all inherited from User
 
-    //fields
-    private String staffID;
-
-    //getters and setters
-
-    public String getStaffID() {
-        return staffID;
-    }
-
-    public void setStaffID(String ID) {
-        staffID = ID;
+    //constructor
+    public CareerCenStaff(String userID, String name, String password, String domain) {
+        super(userID, name, password, domain);
     }
 
     //methods
@@ -26,7 +17,6 @@ public class CareerCenStaff extends User{
         System.out.println(companyRep.getCompanyName() + " has been approved.");
     }
 
-
     public void approveIntern(CompanyRep rep, int index ) {
         Internships internship = rep.getInternships().get(index);
         internship.setOpportunityStatus(OpportunityStatus.APPROVED);
@@ -34,18 +24,18 @@ public class CareerCenStaff extends User{
 
     }
 
-    public void approveWithdrawalRequest(Student student, WithdrawalDecision decision) {
-        student.getStudentApplication().setWithdrawalDecision(decision);  //same thing as before
-        System.out.println(student.getName() + "'s withdrawal decision has been " + decision);
+    public void approveWithdrawalRequest(StudentApplication app, WithdrawalDecision decision) {
+        app.setWithdrawDecision(decision);  //same thing as before
+        System.out.println(app.getStudent().getName() + "'s withdrawal decision has been " + decision);
     }
 
-    public void generateReport(Student student) { //no ability to sort yet!!
-        System.out.println("Student application ID: " + student.getStudentApplication().getApplicationID());
-        System.out.println("Student name: " + student.getStudentId());
-        System.out.println("Number of internships applied: " + student.getStudentApplication().getInternships().size());
-        if (!student.getStudentApplication().getInternships().isEmpty()) {
-            for (int i = 0; i < student.getStudentApplication().getInternships().size(); i++) {
-                Internships internship = student.getStudentApplication().getInternships().get(i);
+    public void generateReport(StudentApplication app) { //no ability to sort yet!!
+        System.out.println("Student application ID: " + app.getApplicationID());
+        System.out.println("Student name: " + app.getStudent().getName());
+        System.out.println("Number of internships applied: " + app.getInternship().size()); //internship is not an array yet! cannot retrieve number of internships
+        if (!app.getInternship().isEmpty()) { //need to initialize Internship object as a array!
+            for (int i = 0; i < app.getInternship().size(); i++) {
+                Internships internship = app.getInternship().get(i);
                 System.out.println((i + 1) + ". " + internship.getTitle() + " - Status: " + internship.getOpportunityStatus());
             }
         } else {
